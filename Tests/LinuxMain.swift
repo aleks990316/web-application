016b3f3010b2467bd0ad0694ed6047c9e3ba1e66
aleks990316 <71783039+aleks990316@users.noreply.github.com>
2020-11-19 17:25:43 +0700

    Add files via upload

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+import web_applicationTests
+
+var tests = [XCTestCaseEntry]()
+tests += web_applicationTests.allTests()
+<<<<<<< HEAD
+XCTMain(tests)
+=======
+XCTMain(tests)
+>>>>>>> 5fb0d50... commit
---
+++ Tests/web-applicationTests/XCTestManifests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+#if !canImport(ObjectiveC)
+public func allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(web_applicationTests.allTests),
+    ]
+}
+<<<<<<< HEAD
+#endif
+=======
+#endif
+>>>>>>> 5fb0d50... commit
